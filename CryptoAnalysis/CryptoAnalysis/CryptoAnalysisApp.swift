0cc6ef2df3731ab599dc03d5401996cb3b804efd
manishparihar948 <manish.parihar948@gmail.com>
2023-07-17 22:05:36 +0200

    Initial Commit

---
+++ CryptoAnalysis/CryptoAnalysis/CryptoAnalysisApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoAnalysisApp.swift
+//  CryptoAnalysis
+//
+//  Created by Manish Parihar on 17.07.23.
+//
+
+import SwiftUI
+
+@main
+struct CryptoAnalysisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
