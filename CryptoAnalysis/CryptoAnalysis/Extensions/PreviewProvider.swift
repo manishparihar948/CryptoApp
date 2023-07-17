074da4af8b21cfbf09a118aff43132891dd2353a
manishparihar948 <manish.parihar948@gmail.com>
2023-07-18 01:26:22 +0200

    Design a row to display coins in a List

---
+++ CryptoAnalysis/CryptoAnalysis/Extensions/PreviewProvider.swift
@@ -0,0 +1,55 @@
+//
+//  PreviewProvider.swift
+//  CryptoAnalysis
+//
+//  Created by Manish Parihar on 18.07.23.
+//
+
+import Foundation
+import SwiftUI
+
+extension PreviewProvider {
+    
+    static var dev: DeveloperPreview {
+        return DeveloperPreview.instance
+    }
+    
+}
+
+// Singleton Class
+// But should not use in production code
+class DeveloperPreview {
+    static let instance = DeveloperPreview()
+    private init() { } // If we have private we can sure that we can only initialize inside the class.
+    
+    let coin = CoinModel(id: "bitcoin",
+                         symbol: "btc",
+                         name: "Bitcoin",
+                         image: "https://assets.coingecko.com/coins/images/1/large/bitcoin.png?1547033579",
+                         currentPrice: 26650,
+                         marketCap: 518679453319,
+                         marketCapRank: 1,
+                         fullyDilutedValuation: 560531483122,
+                         totalVolume: 9704845418,
+                         high24H: 27040,
+                         low24H: 26496,
+                         priceChange24H: -318.81957120988955,
+                         priceChangePercentage24H: -1.18217,
+                         marketCapChange24H: -5445364579.416565,
+                         marketCapChangePercentage24H: -1.03894,
+                         circulatingSupply: 19432037,
+                         totalSupply: 21000000,
+                         maxSupply: 21000000,
+                         ath: 59717,
+                         athChangePercentage:  -55.406,
+                         athDate: "2021-11-10T14:24:11.849Z",
+                         atl: 51.3,
+                         atlChangePercentage: 51812.18469,
+                         atlDate: "2013-07-05T00:00:00.000Z",
+                         lastUpdated: "2023-07-17T21:13:20.058Z",
+                         sparklineIn7D: SparklineIn7D(price:[
+                        30309.441283043412,
+                        30431.81387244306]),
+                         priceChangePercentage24HInCurrency: -1.1821738890776885,
+                         currentHoldings: 1.5)
+}
--- CryptoAnalysis/CryptoAnalysis/Models/CoinModel.swift
+++ CryptoAnalysis/CryptoAnalysis/Models/CoinModel.swift
@@ -48,17 +48,66 @@ import Foundation
      "price_change_percentage_24h_in_currency": -1.1821738890776885
    }
  
+ */
+
+
+// CoinGecko API info
+/*
+ URL: https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=250&page=1&sparkline=true&price_change_percentage=24h
+ 
+ JSON Response:
+ {
+     "id": "bitcoin",
+     "symbol": "btc",
+     "name": "Bitcoin",
+     "image": "https://assets.coingecko.com/coins/images/1/large/bitcoin.png?1547033579",
+     "current_price": 58908,
+     "market_cap": 1100013258170,
+     "market_cap_rank": 1,
+     "fully_diluted_valuation": 1235028318246,
+     "total_volume": 69075964521,
+     "high_24h": 59504,
+     "low_24h": 57672,
+     "price_change_24h": 808.94,
+     "price_change_percentage_24h": 1.39234,
+     "market_cap_change_24h": 13240944103,
+     "market_cap_change_percentage_24h": 1.21837,
+     "circulating_supply": 18704250,
+     "total_supply": 21000000,
+     "max_supply": 21000000,
+     "ath": 64805,
+     "ath_change_percentage": -9.24909,
+     "ath_date": "2021-04-14T11:54:46.763Z",
+     "atl": 67.81,
+     "atl_change_percentage": 86630.1867,
+     "atl_date": "2013-07-06T00:00:00.000Z",
+     "roi": null,
+     "last_updated": "2021-05-09T04:06:09.766Z",
+     "sparkline_in_7d": {
+       "price": [
+         57812.96915967891,
+         57504.33531773738,
+       ]
+     },
+     "price_change_percentage_24h_in_currency": 1.3923423473152687
+   }
  
  */
 
+
+import Foundation
+
+
 struct CoinModel: Identifiable, Codable {
-    // Identifiable because of we having id
     let id, symbol, name: String
     let image: String
     let currentPrice: Double
     let marketCap, marketCapRank, fullyDilutedValuation: Double?
     let totalVolume, high24H, low24H: Double?
-    let priceChange24H, priceChangePercentage24H:Double?, marketCapChange24H:Double?, marketCapChangePercentage24H: Double?
+    let priceChange24H: Double?
+    let priceChangePercentage24H: Double?
+    let marketCapChange24H: Double?
+    let marketCapChangePercentage24H: Double?
     let circulatingSupply, totalSupply, maxSupply, ath: Double?
     let athChangePercentage: Double?
     let athDate: String?
@@ -67,8 +116,7 @@ struct CoinModel: Identifiable, Codable {
     let lastUpdated: String?
     let sparklineIn7D: SparklineIn7D?
     let priceChangePercentage24HInCurrency: Double?
-    let currentHoldings:Double? // How many coins current user is holding
-    
+    let currentHoldings: Double?
     
     enum CodingKeys: String, CodingKey {
         case id, symbol, name, image
@@ -92,25 +140,24 @@ struct CoinModel: Identifiable, Codable {
         case atl
         case atlChangePercentage = "atl_change_percentage"
         case atlDate = "atl_date"
-        case roi
         case lastUpdated = "last_updated"
         case sparklineIn7D = "sparkline_in_7d"
         case priceChangePercentage24HInCurrency = "price_change_percentage_24h_in_currency"
         case currentHoldings
     }
     
-    // It will always give new coinmodel but with updated holdings
-    func updateHoldings(amout: Double) -> CoinModel {
-        return CoinModel(id: id, symbol: symbol, name: name, image: image, currentPrice: currentPrice, marketCap: marketCap, marketCapRank: marketCapRank, fullyDilutedValuation: fullyDilutedValuation, totalVolume: totalVolume, high24H: high24H, low24H: low24H, priceChange24H: priceChange24H, priceChangePercentage24H: priceChangePercentage24H, marketCapChange24H: marketCapChange24H, marketCapChangePercentage24H: marketCapChangePercentage24H, circulatingSupply: circulatingSupply, totalSupply: totalSupply, maxSupply: maxSupply, ath: ath, athChangePercentage: athChangePercentage, athDate: athDate, atl: atl, atlChangePercentage: atlChangePercentage, atlDate: atlDate, lastUpdated: lastUpdated, sparklineIn7D: sparklineIn7D, priceChangePercentage24HInCurrency: priceChangePercentage24HInCurrency, currentHoldings: amout)
+    func updateHoldings(amount: Double) -> CoinModel {
+        return CoinModel(id: id, symbol: symbol, name: name, image: image, currentPrice: currentPrice, marketCap: marketCap, marketCapRank: marketCapRank, fullyDilutedValuation: fullyDilutedValuation, totalVolume: totalVolume, high24H: high24H, low24H: low24H, priceChange24H: priceChange24H, priceChangePercentage24H: priceChangePercentage24H, marketCapChange24H: marketCapChange24H, marketCapChangePercentage24H: marketCapChangePercentage24H, circulatingSupply: circulatingSupply, totalSupply: totalSupply, maxSupply: maxSupply, ath: ath, athChangePercentage: athChangePercentage, athDate: athDate, atl: atl, atlChangePercentage: atlChangePercentage, atlDate: atlDate, lastUpdated: lastUpdated, sparklineIn7D: sparklineIn7D, priceChangePercentage24HInCurrency: priceChangePercentage24HInCurrency, currentHoldings: amount)
     }
     
     var currentHoldingsValue: Double {
         return (currentHoldings ?? 0) * currentPrice
     }
     
-    var rank:Int {
+    var rank: Int {
         return Int(marketCapRank ?? 0)
     }
+    
 }
 
 struct SparklineIn7D: Codable {
